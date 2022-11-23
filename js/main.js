44eca9c98da85de127f3faf116c6983ab61bb573
Leozinsk <leozinsk@gmail.com>
2022-11-23 16:47:05 +0000

    second day

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@ const nascimento = document.querySelector('#birth-date')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    console.log('O nome dgitiado é?',nome.value)
-    console.log('A data dgitiada é?',nascimento.value)
+    const values = [nome.value, nascimento.value]
+    const ano = values[1].slice(0,4)
+    const mes = values[1].slice(5,7)
+    const dia = values[1].slice(-2)
+    console.log('O nome dgitiado é..', values[0])
+    console.log('A data dgitiada é..', values[1])
+    console.log(`A data convertida é.. ${dia}-${mes}-${ano}`)
 })
