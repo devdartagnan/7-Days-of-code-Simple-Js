be1f9b83fa5416373076b1c1f95f0f42873b0082
Leozinsk <leozinsk@gmail.com>
2022-11-24 14:00:36 +0000

    terceiro dia

--- js/main.js
+++ js/main.js
@@ -1,14 +1,34 @@
 const form = document.querySelector('.form')
 const nome = document.querySelector('#name')
 const nascimento = document.querySelector('#birth-date')
+const log = document.querySelector('.log')
+const getLocal = JSON.parse(localStorage.getItem('Cadastrado'))
+const lista = []
+
+
+if (getLocal !== null) {
+    const lastItem = getLocal.slice(-1)
+    //add last log div
+    const novoItem = document.createElement("p")
+    novoItem.innerText = `Nome: ${lastItem[0].nome} | Data: ${lastItem[0].data}`
+    log.appendChild(novoItem)
+    log.classList.toggle('visible')
+}
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const values = [nome.value, nascimento.value]
-    const ano = values[1].slice(0,4)
-    const mes = values[1].slice(5,7)
-    const dia = values[1].slice(-2)
-    console.log('O nome dgitiado é..', values[0])
-    console.log('A data dgitiada é..', values[1])
-    console.log(`A data convertida é.. ${dia}-${mes}-${ano}`)
+    setValues()
 })
+function setValues() {
+    const values = [nome.value, nascimento.value]
+    const ano = values[1].slice(0, 4)
+    const mes = values[1].slice(5, 7)
+    const dia = values[1].slice(-2)
+    const data = `${dia}-${mes}-${ano}`
+    const stringLocal = {
+        nome: values[0],
+        data: data
+    }
+    lista.push(stringLocal)
+    localStorage.setItem('Cadastrado', JSON.stringify(lista))
+    
+}
